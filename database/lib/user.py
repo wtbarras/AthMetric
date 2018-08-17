514cc9e79c233e2a7381f83b5d20508b2c3a999b
wtbarras <williamtbarras@gmail.com>
2018-08-17 13:42:17 -0600

    Make user member variable static

--- database/lib/user.py
+++ database/lib/user.py
@@ -1,4 +1,6 @@
 class User:
-    user_id = null
-    email = null
-    signup_date = null
+    
+    def __init__(self, user_id, email, sign_up_date):
+        self.user_id = user_id
+        self.email = email
+        self.sign_up_date = sign_up_date
