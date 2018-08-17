a79335297f29ab4d9856e95f4fe726b1c69ad9bd
wtbarras <williamtbarras@gmail.com>
2018-08-16 21:51:47 -0600

    Add method to retrieve runs for given user

--- db_utility.py
+++ db_utility.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+# Adds a new run to DB
 def addRunToDb():
     # Connect to database
     conn = sqlite3.connect('rundata.db')
@@ -7,5 +8,16 @@ def addRunToDb():
     conn.execute("INSERT INTO runs VALUES (0, '2018-1-1', 1.1, '1:1:11', 0)")
     # Save (commit) the changes
     conn.commit()
+    # Close connection
+    conn.close()
+
+def getRunsForUser(id):
+    # Connect to database
+    conn = sqlite3.connect('rundata.db')
+    # Get all runs for this user id
+    runs = conn.execute('SELECT * FROM runs WHERE id = ?', (id,))
+    # Print runs
+    for row in runs:
+        print(row)
     # Close connection
     conn.close()
