29166124bc72483df1f17c25924fc6ce9e7a09be
wtbarras <williamtbarras@gmail.com>
2018-08-16 21:32:18 -0600

    Function to add run entry to db

--- db_utility.py
+++ db_utility.py
@@ -1,1 +1,11 @@
 import sqlite3
+
+def addRunToDb():
+    # Connect to database
+    conn = sqlite3.connect('rundata.db')
+    # Insert a new run(currently just dummy data)
+    conn.execute("INSERT INTO runs VALUES (0, '2018-1-1', 1.1, '1:1:11', 0)")
+    # Save (commit) the changes
+    conn.commit()
+    # Close connection
+    conn.close()
