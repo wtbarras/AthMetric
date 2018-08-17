202f85f64c276456cc5f0b1d7ea6216a664e8694
wtbarras <williamtbarras@gmail.com>
2018-08-17 16:04:38 -0600

    Add shoe object

---
+++ database/lib/shoe.py
@@ -0,0 +1,7 @@
+class Shoe:
+    
+    def __init(self, shoe_id, name, total_miles, target_miles)__:
+        self.shoe_id = shoe_id
+        self.name = name
+        self.total_miles = total_miles
+        self.target_miles = target_miles
