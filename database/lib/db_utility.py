4771a6b74f1bde861865a755d088fa836a637bd7
wtbarras <williamtbarras@gmail.com>
2018-08-17 13:24:58 -0600

    Rename objects directory to lib. Mv utility into lib

--- database/lib/db_utility.py
+++ database/lib/db_utility.py
@@ -1,9 +1,17 @@
-import sqlite3
+import sqlite3 as sql
+import objects/user.py
+
+# Add new user
+def addUserToDb(User user):
+    # Open db connection
+    with sqlite3.connect("rundata.db") as conn:
+        curr = conn.cursor()
+        curr.execute("INSERT INTO users VALUES(0, 'tigerbarras@gmail.com', '1-1-11')")
 
 # Adds a new run to DB
 def addRunToDb():
     # Connect to database
-    conn = sqlite3.connect('rundata.db')
+    conn = sql.connect('rundata.db')
     # Insert a new run(currently just dummy data)
     conn.execute("INSERT INTO runs VALUES (0, '2018-1-1', 1.1, '1:1:11', 0)")
     # Save (commit) the changes
@@ -13,7 +21,7 @@ def addRunToDb():
 
 def getRunsForUser(id):
     # Connect to database
-    conn = sqlite3.connect('rundata.db')
+    conn = sql.connect('rundata.db')
     # Get all runs for this user id
     runs = conn.execute('SELECT * FROM runs WHERE id = ?', (id,))
     # Print runs
---
+++ database/lib/user.py
@@ -0,0 +1,4 @@
+class User:
+    user_id = null
+    email = null
+    signup_date = null
