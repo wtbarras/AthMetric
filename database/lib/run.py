78681034f9eb55ec79d70ab60979c80c2585663b
wtbarras <williamtbarras@gmail.com>
2018-08-18 09:38:30 -0600

    Add class for run data

---
+++ database/lib/run.py
@@ -0,0 +1,8 @@
+class Run:
+
+    def __init(self, date, distance, time, user_id, shoe_id)__:
+        this.date = date
+        this.distance = distance
+        this.time = time
+        this.user_id = user_id
+        this.shoe_id = shoe_id
