51a9d5c9aabe796405b23c0b999cfe5d187b6a11
wtbarras <williamtbarras@gmail.com>
2018-08-19 20:46:15 -0600

    Remove shoe id field, add user_id

--- database/lib/shoe.py
+++ database/lib/shoe.py
@@ -1,7 +1,7 @@
 class Shoe:
     
-    def __init__(self, shoe_id, name, total_miles, target_miles):
-        self.shoe_id = shoe_id
+    def __init__(self, name, total_miles, target_miles, user_id):
         self.name = name
         self.total_miles = total_miles
         self.target_miles = target_miles
+        self.user_id = user_id
