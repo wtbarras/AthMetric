fd400e960e3c996a5c1587d8a0d0254f339b1d86
wtbarras <williamtbarras@gmail.com>
2018-08-19 19:44:54 -0600

    Bugfixes

--- database/lib/run.py
+++ database/lib/run.py
@@ -1,8 +1,8 @@
 class Run:
 
-    def __init(self, date, distance, time, user_id, shoe_id)__:
-        this.date = date
-        this.distance = distance
-        this.time = time
-        this.user_id = user_id
-        this.shoe_id = shoe_id
+    def __init__(self, date, distance, time, user_id, shoe_id):
+        self.date = date
+        self.distance = distance
+        self.time = time
+        self.user_id = user_id
+        self.shoe_id = shoe_id
