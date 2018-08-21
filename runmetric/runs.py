369850e0d577dc42129c9a9ec434b4200fa3a73a
wtbarras <williamtbarras@gmail.com>
2018-08-21 11:49:07 -0600

    Add basic create functionalilty

--- runmetric/runs.py
+++ runmetric/runs.py
@@ -20,3 +20,35 @@ def index():
         'SELECT * FROM run'
     ).fetchall()
     return render_template('runs/index.html', runs=runs)
+
+@bp.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        # Get id for logged in user
+        user_id = session.get('user_id')
+        # Get data from form
+        date = request.form['date']
+        distance = request.form['distance']
+        time = request.form['time']
+        shoe_id = request.form['shoe_id']
+
+        # Check if any fields are missing
+        error = None
+        if not date or not distance or not time:
+            error = "Missing fields"
+
+        # If there are errors, display them
+        if error is not None:
+            flash(error)
+        else:
+            # Add run to database
+            db = get_db()
+            db.execute(
+                'INSERT INTO run (date, distace, time, user_id, shoe_id)'
+                ' VALUES (?, ?, ?, ?)',
+                (date, distance, time, user_id, shoe_id)
+                )
+            db.commit()
+            return redirect(url_for('runs.index'))
+    else:
+        return render_template('runs/create.html')
