106cd1cbe0688e9d13831c64242486cdac4d9787
wtbarras <williamtbarras@gmail.com>
2018-08-30 11:34:58 -0600

    Add db inserts to test script

---
+++ tests/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO user (user_id, email, password)
+VALUES (0, "test@test.com", "testpass")
+
+INSERT INTO run (date, distance, time, user_id, shoe_id)
+VALUES ("1-1-2011", 11.11, "01:01:01", 0, 0)
