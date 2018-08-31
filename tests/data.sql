a20818d9d6a7612cd047785c0f41fd27d6d0dba6
wtbarras <williamtbarras@gmail.com>
2018-08-30 19:34:49 -0600

    Fix queries

--- tests/data.sql
+++ tests/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO user (user_id, email, password)
-VALUES (0, "test@test.com", "testpass")
+INSERT INTO user (email, password)
+VALUES ("test@test.com", "testpass");
 
 INSERT INTO run (date, distance, time, user_id, shoe_id)
-VALUES ("1-1-2011", 11.11, "01:01:01", 0, 0)
+VALUES ("1-1-2011", 11.11, "01:01:01", 0, 0);
