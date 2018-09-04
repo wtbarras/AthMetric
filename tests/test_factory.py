be0fe088ecd75ba2ad09841b32f3b6b491357adc
wtbarras <williamtbarras@gmail.com>
2018-09-03 20:04:01 -0600

    Add comments

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,11 +1,11 @@
 from runmetric import create_app
 
-
+# Test to make sure that the TESTING config is not set by default
 def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
 
-
+# Test that test endpoint /hello returns 'Hello, World!'
 def test_hello(client):
     response = client.get('/hello')
     assert response.data == b'Hello, World!'
