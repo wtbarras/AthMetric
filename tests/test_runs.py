e19b0e89f591fc789bbe032fe158e46a99dcfbd7
wtbarras <williamtbarras@gmail.com>
2018-09-04 09:19:32 -0600

    Add test to make sure logged out users are redirected to login page

--- tests/test_runs.py
+++ tests/test_runs.py
@@ -2,6 +2,23 @@ import pytest
 from runmetric.db import get_db
 
 def test_index(client, auth):
+    # Make sure that the log in page is displayed if we haven't logged in yet
     response = client.get('/', follow_redirects=True)
     assert b"Log In" in response.data
     assert b"Register" in response.data
+
+    # Log in
+    auth.login()
+    # Make sure we get the main page now that we're logged in
+    response = client.get('/')
+    assert b'Logout' in response.data
+
+@pytest.mark.parametrize(('path'),(
+    '/create',
+    '/1/delete',
+    '/1/update',
+))
+def test_login_required(client, path):
+    # Make sure that these paths redirect non logged in users to the login page
+    response = client.post(path)
+    assert response.headers['Location'] == 'http://localhost/auth/login'
