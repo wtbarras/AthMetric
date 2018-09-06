6ef6bef17ca6a89e4b1b6d7000ecaca9112fc4ce
wtbarras <williamtbarras@gmail.com>
2018-09-06 15:13:35 -0600

    Full add run post implementation

--- runmetric/shoes.py
+++ runmetric/shoes.py
@@ -1,8 +1,9 @@
 from flask import (
-    Blueprint
+    Blueprint, request, session
 )
 
 from runmetric.auth import login_required
+from runmetric.db import add_shoe
 
 bp = Blueprint('shoes', __name__, url_prefix='/shoes')
 
@@ -15,7 +16,20 @@ def display_shoes():
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create_shoe():
-    return "CREATE SHOES"
+    if request.method == 'POST':
+        # Get current user's id
+        user_id = session.get('user_id')
+
+        # Get data from request
+        name = request.form['name']
+        total_miles = request.form['total_miles']
+        target_miles = request.form['target_miles']
+
+        # Add shoe to db
+        add_shoe(name, total_miles, target_miles, user_id)
+        return ""
+    else:
+        return "CREATE SHOES"
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
