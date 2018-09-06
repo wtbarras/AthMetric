445ec60899562d53441f35fab4c736da145f2408
wtbarras <williamtbarras@gmail.com>
2018-09-06 16:52:51 -0600

    Implement update shoe test

--- tests/test_shoes.py
+++ tests/test_shoes.py
@@ -1,6 +1,6 @@
 import pytest
 
-from runmetric.db import count_shoes
+from runmetric.db import count_shoes, get_shoe_by_id
 
 # Make sure GET method endpoints cannot be accessed when logged out
 @pytest.mark.parametrize(('path'), (
@@ -38,3 +38,22 @@ def test_create_run(client, auth, app):
     with app.app_context():
         post_shoe_count = count_shoes()
     assert post_shoe_count == pre_shoe_count + 1
+
+# Test updating a shoe entry
+def test_update_shoe(auth, client, app):
+    # Log in so that route actually works
+    auth.login()
+
+    response = client.post('/shoes/2/update')
+    assert response.status_code == 404
+
+    new_name = 'Newton Kismet 2'
+    new_total_miles = 15
+    new_target_miles = 500
+
+    response = client.post('/shoes/1/update', data={'name': new_name, 'total_miles': new_total_miles, 'target_miles': new_target_miles})
+    with app.app_context():
+        shoe = get_shoe_by_id(1)
+        assert shoe['name'] == new_name
+        assert shoe['total_miles'] == new_total_miles
+        assert shoe['target_miles'] == new_target_miles
