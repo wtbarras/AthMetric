aa22ffbebfe5c650cf910490be7011eacfd5fa53
wtbarras <williamtbarras@gmail.com>
2018-09-06 15:13:56 -0600

    Add test for add run

--- tests/test_shoes.py
+++ tests/test_shoes.py
@@ -1,5 +1,7 @@
 import pytest
 
+from runmetric.db import count_shoes
+
 # Make sure GET method endpoints cannot be accessed when logged out
 @pytest.mark.parametrize(('path'), (
     ('/shoes/'),
@@ -19,3 +21,20 @@ def test_login_required_shoes_get(client, path):
 def test_login_required_shoes_post(client, path):
     response = client.post(path)
     assert response.headers['Location'] == 'http://localhost/auth/login'
+
+# Test the creation of a new shoe entry
+def test_create_run(client, auth, app):
+    # Log in so that route actually works
+    auth.login()
+
+    # Get current number of shoes in db
+    with app.app_context():
+        pre_shoe_count = count_shoes()
+
+    # Add shoe
+    path = '/shoes/create'
+    client.post(path, data={'name': 'Newton Kismet 2', 'total_miles': '0', 'target_miles': '500'})
+
+    with app.app_context():
+        post_shoe_count = count_shoes()
+    assert post_shoe_count == pre_shoe_count + 1
