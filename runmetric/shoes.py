3c5c0989a7a6e9388cda4a286754e24dc630f5de
wtbarras <williamtbarras@gmail.com>
2018-09-05 22:17:09 -0600

    Add methods to shoe routes

--- runmetric/shoes.py
+++ runmetric/shoes.py
@@ -7,22 +7,22 @@ from runmetric.auth import login_required
 bp = Blueprint('shoes', __name__, url_prefix='/shoes')
 
 # Shoes root. Display all of a user's shoes
-@bp.route('/')
+@bp.route('/', methods=('GET',))
 @login_required
 def display_shoes():
     return "SHOES"
 
-@bp.route('/create')
+@bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create_shoe():
     return "CREATE SHOES"
 
-@bp.route('/<int:id>/update')
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
 def update_shoe(id):
     return 'UPDATE SHOES'
 
-@bp.route('/<int:id>/delete')
+@bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete_shoe(id):
     return 'DELETE SHOES'
