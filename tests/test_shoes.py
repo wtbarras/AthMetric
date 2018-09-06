06672d547b9e64d01016c5835a760e97c97189ae
wtbarras <williamtbarras@gmail.com>
2018-09-06 14:04:15 -0600

    Test POST method endpoints require login

--- tests/test_shoes.py
+++ tests/test_shoes.py
@@ -1,6 +1,6 @@
 import pytest
 
-# Make sure get methods cannot be accessed when logged out
+# Make sure GET method endpoints cannot be accessed when logged out
 @pytest.mark.parametrize(('path'), (
     ('/shoes/'),
     ('/shoes/create'),
@@ -8,4 +8,14 @@ import pytest
 ))
 def test_login_required_shoes_get(client, path):
     response = client.get(path)
+    assert response.headers['Location'] == 'http://localhost/auth/login'
+
+# Make sure POST method endpoints cannot be accessed when logged out
+@pytest.mark.parametrize(('path'), (
+    ('/shoes/create'),
+    ('/shoes/1/update'),
+    ('/shoes/1/delete')
+))
+def test_login_required_shoes_post(client, path):
+    response = client.post(path)
     assert response.headers['Location'] == 'http://localhost/auth/login'
