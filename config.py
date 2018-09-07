d292b3b080c44337a1c863936bc974156988fe9a
wtbarras <williamtbarras@gmail.com>
2018-09-07 17:05:14 -0600

    Add config file for gcloud

---
+++ config.py
@@ -0,0 +1,14 @@
+"""
+This file contains all of the configuration values for the application.
+Update this file with the values for your specific Google Cloud project.
+You can create and manage projects at https://console.developers.google.com
+"""
+
+import os
+
+# The secret key is used by Flask to encrypt session cookies.
+SECRET_KEY = 'secret'
+
+# Google Cloud Project ID. This can be found on the 'Overview' page at
+# https://console.developers.google.com
+PROJECT_ID = 'runmetric-215721'
