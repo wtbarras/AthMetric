bb08ce3ba8bcb1dba5685779034357d719155310
William <williamtbarras@gmail.com>
2018-11-25 12:44:48 -0700

    Initial model fil for lifting_activity

---
+++ runmetric/models/database/lifting_activity.py
@@ -0,0 +1,10 @@
+class LiftingActivity:
+
+    def __init__(self, date, start_time, end_time, lift_data, lift_id):
+        self.description = desc
+        self.date = date
+        self.start_time = start_time
+        self.end_time = end_time
+        self.lifting_data = lift_data
+        self.lift_id = lift_id
+        self.user_id = user_id
