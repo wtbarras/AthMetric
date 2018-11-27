0141d2b2b5adac6c812618cfbc8b41c3a93b6d05
William <williamtbarras@gmail.com>
2018-11-26 22:03:07 -0700

    Remove unused imports

--- runmetric/activities.py
+++ runmetric/activities.py
@@ -5,12 +5,7 @@ from werkzeug.exceptions import abort
 
 from runmetric.auth import login_required
 from runmetric.models.database.run import Run
-from runmetric.db import get_db
-from runmetric.db import add_run
-from runmetric.db import update_run
-from runmetric.db import get_run_by_id
-from runmetric.db import get_runs_for_user
-from runmetric.db import delete_run
+
 
 bp = Blueprint('activities', __name__, url_prefix='/activities')
 
